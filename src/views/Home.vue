edb6fbb65dbca3a9da2171349dbc630b12299bc9
bayumen <87920213+bayumen@users.noreply.github.com>
2021-07-31 23:08:46 +0700

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="container">
+      <Hero />
+
+      <div class="row mt-4">
+        <div class="col">
+          <h2>
+            
+            
+          </h2>
+        </div>
+        
+          
+        
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    Hero,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      
+  },
+};
+</script>
